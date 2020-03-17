e7b118e08c9ddcfbf1db356aefef54fa73389ee8
Jongko <bazinga940@gmail.com>
2020-03-17 23:47:30 +0900

    xcode project 생성
     메모앱을 만들어보자

---
+++ JH-memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JH-memo
+//
+//  Created by Jongho Lee on 2020/03/17.
+//  Copyright © 2020 Jongho Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ JH-memoTests/JH_memoTests.swift
@@ -0,0 +1,34 @@
+//
+//  JH_memoTests.swift
+//  JH-memoTests
+//
+//  Created by Jongho Lee on 2020/03/17.
+//  Copyright © 2020 Jongho Lee. All rights reserved.
+//
+
+import XCTest
+@testable import JH_memo
+
+class JH_memoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
