a803515781c5a53119940a1c6c7f8fd42552ca8b
bazinga94 <bazinga940@gmail.com>
2020-03-29 00:54:34 +0900

    edit 화면 scroll view 추가중

--- JH-memo/Home/HomeViewController.swift
+++ JH-memo/Home/HomeViewController.swift
@@ -11,39 +11,45 @@ import UIKit
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
 	@IBOutlet weak var tableView: UITableView!
-	var delegate: SendDataDelegate?
-	var rowString: String?
-
-	var sampleCellTitle = ["JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH", "JH"]
+	var homeCellTitle: [String] = []
 
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return sampleCellTitle.count
+		return homeCellTitle.count
 	}
 
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		var cell: JHTableViewCell
 		let row = indexPath.row
 		cell = (tableView.dequeueReusableCell(withIdentifier: "JHCell") as? JHTableViewCell)!
-		cell.title.text = sampleCellTitle[row]
+		cell.title.text = homeCellTitle[row]
 		return cell
 	}
 
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		tableView.deselectRow(at: indexPath, animated: false)
-		let storyBoard: UIStoryboard! = UIStoryboard(name: "Main", bundle: nil)
-		let editViewController: EditViewController = (storyBoard.instantiateViewController(withIdentifier: "Edit") as? EditViewController)!
-		self.navigationController?.pushViewController(editViewController, animated: true)
-		self.rowString = String(indexPath.row)
+//		let storyBoard: UIStoryboard! = UIStoryboard(name: "Main", bundle: nil)
+//		let editViewController: EditViewController = (storyBoard.instantiateViewController(withIdentifier: "Edit") as? EditViewController)!
+//		self.navigationController?.pushViewController(editViewController, animated: true)
 	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		tableView.dataSource = self
 		tableView.delegate = self
-		// Do any additional setup after loading the view.
+	}
+
+	@IBAction func addMemo(_ sender: Any) {
+		let storyBoard: UIStoryboard! = UIStoryboard(name: "Main", bundle: nil)
+		let editViewController: EditViewController = (storyBoard.instantiateViewController(withIdentifier: "Edit") as? EditViewController)!
+		self.navigationController?.pushViewController(editViewController, animated: true)
 	}
 
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//		if segue.destination is EditViewController
+//		{
+//			let vc = segue.destination as? EditViewController
+//			// TODO: - 넘겨줄 데이터 작업
+//		}
 	}
 }
 
